7ab651c52a9a67de40a9421cf72e3fada90a0619
YaizaSirena <yaizamartos@hotmail.com>
2021-04-20 15:57:20 +0200

    Creada DBConnect. Creando DAL Pokegotchi (SelectUsuario InsertUsuario CrearParametro)

---
+++ RetoPokeGotchi/Models/DALPokegotchi.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace RetoPokeGotchi.Models
+{
+    public class DALPokegotchi
+    {
+        DBConnect conexion;
+        
+        public DALPokegotchi()
+        {
+            conexion = new DBConnect();
+        }
+
+        public Usuario SelectUsuario(string nombreIntroducido)
+        {
+            Usuario usuarioExistente = new Usuario();
+            try
+            {
+                string sql = @"select NombreUsuario from Usuario where NombreUsuario = @pNombreUsuario";
+                SqlCommand cmd = new SqlCommand(sql, conexion.Conexion);
+                SqlParameter pNombreUsuario = new SqlParameter("@pNombreUsuario", nombreIntroducido);
+                cmd.Parameters.Add(pNombreUsuario);
+
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    usuarioExistente.NombreUsuario = nombreIntroducido;
+                }
+                dr.Close();
+            }
+                catch (Exception error)
+                {
+                    
+                }
+            return usuarioExistente;
+        }
+
+
+        public void InsertarUsuario(Usuario usuario)
+        {
+            try
+            {
+                string sql = @"insert into Usuario (NombreUsuario)   values('@pNombreUsuario')";
+                SqlCommand cmd = new SqlCommand(sql, conexion.Conexion);
+
+                cmd.Parameters.Add(CrearParametro("@pNombreUsuario", System.Data.SqlDbType.VarChar, 15, usuario.NombreUsuario ));
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception error)
+            {
+                
+            }
+        }
+
+
+        public SqlParameter CrearParametro(string pName, System.Data.SqlDbType type, int size, object value)
+        {
+            SqlParameter param = new SqlParameter(pName, type, size);
+            param.Value = value;
+            return param;
+        }
+
+    }
+}
---
+++ RetoPokeGotchi/Models/DBConnect.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace RetoPokeGotchi.Models
+{
+    public class DBConnect
+    {
+        System.Data.SqlClient.SqlConnection conexion = null;
+
+        public SqlConnection Conexion { get => conexion; set => conexion = value; }
+
+
+        public DBConnect()
+        {
+            Conexion = new SqlConnection("Data Source =DESKTOP-GSHP0S4; Initial Catalog = RetoPokeGotchi; Integrated Security = true;");
+            Conexion.Open();
+        }
+        
+       
+       
+
+    }
+}
